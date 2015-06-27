710f39359903e24219a16b44d84b37b3bdb8c419
Aditi <aditi.medha96@gmail.com>
2015-06-27 17:16:22 +0530

    Add isValidMove.py

---
+++ isValidMove.py
@@ -0,0 +1,6 @@
+def isValidMove(board, move):
+    blankx, blanky = getBlankPosition(board)
+    return (move == UP and blanky != len(board[0]) - 1) or \
+           (move == DOWN and blanky != 0) or \
+           (move == LEFT and blankx != len(board) - 1) or \
+           (move == RIGHT and blankx != 0)
