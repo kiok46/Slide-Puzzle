942705468b2181f70e198086375f7bfa79513f66
Aditi <aditi.medha96@gmail.com>
2015-06-27 17:57:03 +0530

    Add getRandomMove.py

---
+++ getRandomMove.py
@@ -0,0 +1,13 @@
+def getRandomMove(board, lastMove=None):
+    validMoves = [UP, DOWN, LEFT, RIGHT]
+
+    if lastMove == UP or not isValidMove(board, DOWN):
+        validMoves.remove(DOWN)
+    if lastMove == DOWN or not isValidMove(board, UP):
+        validMoves.remove(UP)
+    if lastMove == LEFT or not isValidMove(board, RIGHT):
+        validMoves.remove(RIGHT)
+    if lastMove == RIGHT or not isValidMove(board, LEFT):
+        validMoves.remove(LEFT)
+
+    return random.choice(validMoves)
